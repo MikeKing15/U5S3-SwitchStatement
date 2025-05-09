617f79f625c7adb8c0aded39a4abd61c72496136
MikeKing15 <mikeking11152004@gmail.com>
2025-05-08 23:38:10 -0400

    comment

--- src/main/java/partA/ex01/Switch.java
+++ src/main/java/partA/ex01/Switch.java
@@ -1,19 +1,37 @@
 package partA.ex01;
 
+import java.util.Scanner;
+
 public class Switch {
 
-    public static String switchStatement(){
+    public static String switchStatement() {
         String response = "";
 
         /* Put your code in between these comments : Top */
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a month number: ");
+        int month = scanner.nextInt();
 
-
+        switch (month) {
+            case 1: response = "January"; break;
+            case 2: response = "February"; break;
+            case 3: response = "March"; break;
+            case 4: response = "April"; break;
+            case 5: response = "May"; break;
+            case 6: response = "June"; break;
+            case 7: response = "July"; break;
+            case 8: response = "August"; break;
+            case 9: response = "September"; break;
+            case 10: response = "October"; break;
+            case 11: response = "November"; break;
+            case 12: response = "December"; break;
+            default: response = "Invalid month"; break;
+        }
         /* Put your code in between these comments : Bottom */
-
-
 
         return response;
     }
+
     public static void main(String args[]) {
         String switchOutput = switchStatement();
         System.out.println(switchOutput);
--- src/main/java/partB/ex01/IfLight.java
+++ src/main/java/partB/ex01/IfLight.java
@@ -1,21 +1,30 @@
 package partB.ex01;
 
+import java.util.Scanner;
+
 public class IfLight {
-    public static String ifLight(){
+    public static String ifLight() {
         String response = "";
 
         /* Put your code in between these comments : Top */
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a color code: ");
+        int colorCode = scanner.nextInt();
 
-
-
-
-
+        if (colorCode == 1) {
+            response = "Next Traffic Light is green";
+        } else if (colorCode == 2) {
+            response = "Next Traffic Light is yellow";
+        } else if (colorCode == 3) {
+            response = "Next Traffic Light is red";
+        } else {
+            response = "Invalid color";
+        }
         /* Put your code in between these comments : Bottom */
-
-
 
         return response;
     }
+
     public static void main(String args[]) {
         String switchOutput = ifLight();
         System.out.println(switchOutput);
